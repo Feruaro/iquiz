bc75b01c6af6a0bac0064b54ca4ca29eac40f9e3
fernanda <fernandaruaro26@hotmail.com>
2025-03-19 17:47:13 -0300

    feat: cria estrutura do projeto

---
+++ iQuiz/iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Fernanda Ayres Ruaro on 19/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
